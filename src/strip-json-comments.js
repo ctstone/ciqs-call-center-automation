56c62948f309ae1a05640727727bfc75f6d565ba
Chris Stone <chstone@microsoft.com>
2017-04-25 19:21:22 -0400

    fixed json comments

---
+++ src/strip-json-comments.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const INPUT = path.resolve(__dirname, '../core/azuredeploy.src.json');
+const OUTPUT = path.resolve(__dirname, '../core/azuredeploy.json');
+
+fs.readFile(INPUT, 'utf8', (err, text) => {
+  if (err) throw err;
+
+  text = removeComments(text);
+
+  fs.writeFile(OUTPUT, text, (err) => {
+    if (err) throw err;
+  });
+});
+
+function removeComments(text) {
+  return text
+    .replace(/^\s*\/\/.*$/gm, '')
+    .replace(/^\s*\/\*(\s|\S)*?\*\/\s*$/gm, '');
+}

5cd7a5f0318afd13bd355c30a7bbdab67318dc94
Chris Stone <chstone@microsoft.com>
2017-05-12 10:05:24 -0400

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,15 @@ const SOURCE = 'src';
 const DEST_EXT = '/Source/data-science/CIQS/Product/Source/Patterns/Data/call-center-automation';
 const DEST = '.';
 
-gulp.task('ciqs', () => {
-  return gulp.src('+(assets|core)/**', { cwd: SOURCE })
-    .pipe(gulp.dest('', { cwd: DEST }));
-});
-
-gulp.task('arm', ['ciqs'], () => {
-  return gulp.src('core/azuredeploy.json', { cwd: DEST })
+gulp.task('arm', () => {
+  return gulp.src('arm/**/*.json', { cwd: SOURCE })
     .pipe(replace(/^\s*\/\/.*$/gm, ''))
     .pipe(replace(/^\s*\/\*(\s|\S)*?\*\/\s*$/gm, ''))
     .pipe(gulp.dest('core', { cwd: DEST }));
 });
 
-gulp.task('markdown', ['ciqs'], () => {
-  return gulp.src('core/md/**', { cwd: DEST })
+gulp.task('markdown', () => {
+  return gulp.src('md/**/*.md', { cwd: SOURCE })
     .pipe(replace('../../assets', '{PatternAssetBaseUrl}'))
     .pipe(gulp.dest('core/md', { cwd: DEST }));
 });
